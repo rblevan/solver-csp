f873c2398aa2c0d57c70af004bd1dc0b788fd063
RIBOULEAU Evan <evan.ribouleau@etu.univ-poitiers.fr>
2026-02-26 10:10:06 +0100

    constructor added and method added

--- src/main/java/up/csp/Domain.java
+++ src/main/java/up/csp/Domain.java
@@ -14,12 +14,15 @@ public class Domain {
 	 */
 	public Domain(int min, int max) {
 		// TODO - implement Domain.Domain
-		throw new UnsupportedOperationException();
+		this.min = min;
+		this.max = max;
+		this.presence = new boolean[max - min + 1];
 	}
 
 	/**
 	 * 
 	 * @param value
+	 * This method remove a integer to the domain
 	 */
 	public void removeValue(int value) {
 		// TODO - implement Domain.removeValue
@@ -29,10 +32,15 @@ public class Domain {
 	/**
 	 * 
 	 * @param value
+	 * This method watch if a integer is in the domain
 	 */
 	public boolean contains(int value) {
 		// TODO - implement Domain.contains
-		throw new UnsupportedOperationException();
+		if (this.presence[this.max - value] == true) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public int size() {
